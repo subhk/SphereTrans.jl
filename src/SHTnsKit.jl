9bc8819ec9a302c29a83ee8ad50c6a43113f8db3
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-10 18:46:31 +0300

    Update SHTnsKit.jl

--- src/SHTnsKit.jl
+++ src/SHTnsKit.jl
@@ -1,9 +1,12 @@
 module SHTnsKit
 
 export SHTnsConfig, create_config, set_grid, sh_to_spat, spat_to_sh, free_config,
-       get_lmax, get_mmax, get_nlat, get_nphi, get_nlm, lmidx
+       get_lmax, get_mmax, get_nlat, get_nphi, get_nlm, lmidx,
+       allocate_spectral, allocate_spatial, analyze, analyze!, synthesize, synthesize!,
+       analyze_gpu, synthesize_gpu
 
 include("api.jl")
+include("highlevel.jl")
 
 include("utils.jl")
 
