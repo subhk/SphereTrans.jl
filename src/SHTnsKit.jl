b453643419574337f78ef1335a8f69d189eb111d
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-24 00:11:33 +0300

    Update SHTnsKit.jl

--- src/SHTnsKit.jl
+++ src/SHTnsKit.jl
@@ -13,6 +13,7 @@ include("transform.jl")
 
 export SHTConfig, create_gauss_config, destroy_config
 export analysis, synthesis
+export spat_to_SH, SH_to_spat, spat_to_SH_l, SH_to_spat_l, spat_to_SH_ml, SH_to_spat_ml, SH_to_point
 export nlm_calc, nlm_cplx_calc, LM_index, LiM_index, im_from_lm
 
 end # module SHTnsKit
