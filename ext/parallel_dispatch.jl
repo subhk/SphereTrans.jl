dae7a71e1a0b8ade3ba9ba590f85ec13f4b6f54a
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-26 16:08:02 +0300

    Update parallel_dispatch.jl

--- ext/parallel_dispatch.jl
+++ ext/parallel_dispatch.jl
@@ -24,3 +24,35 @@ function SHTnsKit.synthesis(cfg::SHTnsKit.SHTConfig, Alm::AbstractMatrix;
                 
     return SHTnsKit.dist_synthesis(cfg, PencilArrays.PencilArray(Alm); prototype_θφ, real_output)
 end
+
+##########
+# Vector/QST dispatch for PencilArrays
+##########
+
+function SHTnsKit.spat_to_SHsphtor(cfg::SHTnsKit.SHTConfig, Vtθφ::PencilArrays.PencilArray, Vpθφ::PencilArrays.PencilArray; use_tables=cfg.use_plm_tables)
+    return SHTnsKit.dist_spat_to_SHsphtor(cfg, Vtθφ, Vpθφ; use_tables)
+end
+
+function SHTnsKit.SHsphtor_to_spat(cfg::SHTnsKit.SHTConfig, Slm::PencilArrays.PencilArray, Tlm::PencilArrays.PencilArray; 
+                                   prototype_θφ::PencilArrays.PencilArray, real_output::Bool=true)
+    return SHTnsKit.dist_SHsphtor_to_spat(cfg, Slm, Tlm; prototype_θφ, real_output)
+end
+
+function SHTnsKit.SHsphtor_to_spat(cfg::SHTnsKit.SHTConfig, Slm::AbstractMatrix, Tlm::AbstractMatrix; 
+                                   prototype_θφ::PencilArrays.PencilArray, real_output::Bool=true)
+    return SHTnsKit.dist_SHsphtor_to_spat(cfg, Slm, Tlm; prototype_θφ, real_output)
+end
+
+function SHTnsKit.spat_to_SHqst(cfg::SHTnsKit.SHTConfig, Vrθφ::PencilArrays.PencilArray, Vtθφ::PencilArrays.PencilArray, Vpθφ::PencilArrays.PencilArray)
+    return SHTnsKit.dist_spat_to_SHqst(cfg, Vrθφ, Vtθφ, Vpθφ)
+end
+
+function SHTnsKit.SHqst_to_spat(cfg::SHTnsKit.SHTConfig, Qlm::PencilArrays.PencilArray, Slm::PencilArrays.PencilArray, Tlm::PencilArrays.PencilArray; 
+                                prototype_θφ::PencilArrays.PencilArray, real_output::Bool=true)
+    return SHTnsKit.dist_SHqst_to_spat(cfg, Qlm, Slm, Tlm; prototype_θφ, real_output)
+end
+
+function SHTnsKit.SHqst_to_spat(cfg::SHTnsKit.SHTConfig, Qlm::AbstractMatrix, Slm::AbstractMatrix, Tlm::AbstractMatrix; 
+                                prototype_θφ::PencilArrays.PencilArray, real_output::Bool=true)
+    return SHTnsKit.dist_SHqst_to_spat(cfg, Qlm, Slm, Tlm; prototype_θφ, real_output)
+end
