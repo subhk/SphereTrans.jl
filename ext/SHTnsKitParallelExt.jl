4e9a9e4b2b5cc5b6bacd0cdd2f9b2b1b21161669
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-27 00:31:33 +0300

    Update SHTnsKitParallelExt.jl

--- ext/SHTnsKitParallelExt.jl
+++ ext/SHTnsKitParallelExt.jl
@@ -38,18 +38,27 @@ function _get_or_plan(kind::Symbol, A)
 end
 
 
-# Local aliases for commonly used PencilArrays/PencilFFTs functions
-const communicator = PencilArrays.communicator
-const allocate = PencilArrays.allocate
-const globalindices = PencilArrays.globalindices
+# Local shims for commonly used PencilArrays/PencilFFTs functions (version-agnostic)
+const _has_pa_communicator = isdefined(PencilArrays, :communicator)
+const _has_pa_allocate     = isdefined(PencilArrays, :allocate)
+const _has_pa_globalidx    = isdefined(PencilArrays, :globalindices)
 
-const plan_fft = PencilFFTs.plan_fft
-const fft = PencilFFTs.fft
-const ifft = PencilFFTs.ifft
-const plan_rfft = PencilFFTs.plan_rfft
-const plan_irfft = PencilFFTs.plan_irfft
-const rfft = PencilFFTs.rfft
-const irfft = PencilFFTs.irfft
+communicator(A) = _has_pa_communicator ? (PencilArrays.communicator)(A) : (
+    isdefined(PencilArrays, :comm) ? (PencilArrays.comm)(A) : error("PencilArrays communicator not found"))
+
+allocate(args...; kwargs...) = _has_pa_allocate ? (PencilArrays.allocate)(args...; kwargs...) : error("PencilArrays.allocate not found")
+
+globalindices(A, dim) = _has_pa_globalidx ? (PencilArrays.globalindices)(A, dim) : (
+    isdefined(PencilArrays, :global_indices) ? (PencilArrays.global_indices)(A, dim) : error("PencilArrays.globalindices not found"))
+
+# FFT helpers
+plan_fft(A; dims=:) = PencilFFTs.plan_fft(A; dims=dims)
+fft(A, p) = PencilFFTs.fft(A, p)
+ifft(A, p) = PencilFFTs.ifft(A, p)
+plan_rfft(A; dims=:) = PencilFFTs.plan_rfft(A; dims=dims)
+plan_irfft(A; dims=:) = PencilFFTs.plan_irfft(A; dims=dims)
+rfft(A, p) = PencilFFTs.rfft(A, p)
+irfft(A, p) = PencilFFTs.irfft(A, p)
 
 
 include("parallel_diagnostics.jl")
