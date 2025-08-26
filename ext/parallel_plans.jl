e060431befdf111fbcff833a2664c4129017213d
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-26 17:09:57 +0300

    Update parallel_plans.jl

--- ext/parallel_plans.jl
+++ ext/parallel_plans.jl
@@ -4,32 +4,32 @@
 
 struct DistAnalysisPlan
     cfg::SHTnsKit.SHTConfig
-    prototype_θφ::PencilArrays.PencilArray
+    prototype_θφ::PencilArray
     use_rfft::Bool
 end
 
-DistAnalysisPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArrays.PencilArray; use_rfft::Bool=false) = DistAnalysisPlan(cfg, prototype_θφ, use_rfft)
+DistAnalysisPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArray; use_rfft::Bool=false) = DistAnalysisPlan(cfg, prototype_θφ, use_rfft)
 
 struct DistPlan
     cfg::SHTnsKit.SHTConfig
-    prototype_θφ::PencilArrays.PencilArray
+    prototype_θφ::PencilArray
     use_rfft::Bool
 end
 
-DistPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArrays.PencilArray; use_rfft::Bool=false) = DistPlan(cfg, prototype_θφ, use_rfft)
+DistPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArray; use_rfft::Bool=false) = DistPlan(cfg, prototype_θφ, use_rfft)
 
 struct DistSphtorPlan
     cfg::SHTnsKit.SHTConfig
-    prototype_θφ::PencilArrays.PencilArray
+    prototype_θφ::PencilArray
     use_rfft::Bool
 end
 
-DistSphtorPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArrays.PencilArray; with_spatial_scratch::Bool=false, use_rfft::Bool=false) = DistSphtorPlan(cfg, prototype_θφ, use_rfft)
+DistSphtorPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArray; with_spatial_scratch::Bool=false, use_rfft::Bool=false) = DistSphtorPlan(cfg, prototype_θφ, use_rfft)
 
 struct DistQstPlan
     cfg::SHTnsKit.SHTConfig
-    prototype_θφ::PencilArrays.PencilArray
+    prototype_θφ::PencilArray
     use_rfft::Bool
 end
 
-DistQstPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArrays.PencilArray; with_spatial_scratch::Bool=false, use_rfft::Bool=false) = DistQstPlan(cfg, prototype_θφ, use_rfft)
+DistQstPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArray; with_spatial_scratch::Bool=false, use_rfft::Bool=false) = DistQstPlan(cfg, prototype_θφ, use_rfft)
