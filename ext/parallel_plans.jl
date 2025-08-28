4900bf4424a4d8fc4abb273c84faabb3d7e73fe7
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-28 12:20:44 +0300

    Update parallel_plans.jl

--- ext/parallel_plans.jl
+++ ext/parallel_plans.jl
@@ -22,14 +22,36 @@ struct DistSphtorPlan
     cfg::SHTnsKit.SHTConfig
     prototype_θφ::PencilArray
     use_rfft::Bool
+    with_spatial_scratch::Bool
+    spatial_scratch::Union{Nothing,Tuple{PencilArray,PencilArray}}
 end
 
-DistSphtorPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArray; with_spatial_scratch::Bool=false, use_rfft::Bool=false) = DistSphtorPlan(cfg, prototype_θφ, use_rfft)
+function DistSphtorPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArray; with_spatial_scratch::Bool=false, use_rfft::Bool=false)
+    scratch = if with_spatial_scratch
+        # Pre-allocate complex spatial scratch buffers for IFFT operations
+        scratch_θ = allocate(prototype_θφ; dims=(:θ,:k), eltype=ComplexF64)
+        scratch_φ = allocate(prototype_θφ; dims=(:θ,:k), eltype=ComplexF64)
+        (scratch_θ, scratch_φ)
+    else
+        nothing
+    end
+    return DistSphtorPlan(cfg, prototype_θφ, use_rfft, with_spatial_scratch, scratch)
+end
 
 struct DistQstPlan
     cfg::SHTnsKit.SHTConfig
     prototype_θφ::PencilArray
     use_rfft::Bool
+    with_spatial_scratch::Bool
+    spatial_scratch::Union{Nothing,PencilArray}
 end
 
-DistQstPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArray; with_spatial_scratch::Bool=false, use_rfft::Bool=false) = DistQstPlan(cfg, prototype_θφ, use_rfft)
+function DistQstPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArray; with_spatial_scratch::Bool=false, use_rfft::Bool=false)
+    scratch = if with_spatial_scratch
+        # Pre-allocate complex spatial scratch buffer for scalar IFFT operations
+        allocate(prototype_θφ; dims=(:θ,:k), eltype=ComplexF64)
+    else
+        nothing
+    end
+    return DistQstPlan(cfg, prototype_θφ, use_rfft, with_spatial_scratch, scratch)
+end
